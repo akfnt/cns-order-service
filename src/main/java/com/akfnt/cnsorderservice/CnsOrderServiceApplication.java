b20d3ba05cb900579d5347bf507198e886338670
hjPark <hj.park@navercorp.com>
2024-04-08 13:45:04 +0900

    define custom configuration for Inter-service communication

--- src/main/java/com/akfnt/cnsorderservice/CnsOrderServiceApplication.java
+++ src/main/java/com/akfnt/cnsorderservice/CnsOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.akfnt.cnsorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CnsOrderServiceApplication {
 
 	public static void main(String[] args) {
